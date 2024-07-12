729bf0d948ebce0cfd64fa41dd75421ad8af49c0
margarete86 <margarete.sane@gmail.com>
2024-07-12 12:19:43 -0300

    Finalizado

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-let nomeUsuário = prompt("Qual o seu nome?");
-let elemento = document.querySelector("#nome-usuário");
+let nomeUsuário = prompt ("Qual o seu nome?");
+let elemento = document. querySelector("#nomeUsuário");
 elemento.textContent = nomeUsuário;
-while (nomeUsuário == ""){
-    nomeUsuário = prompt("Qual o seu nome?");
-}
-
-if (nomeUsuário == null){
-    elemento.textContent = 'seja muito bem vindo.';
-}else{
-    elemento.textContent = nomeUsuário;
-}
